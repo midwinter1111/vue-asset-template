7c4a3b87fc00a95f633b7744c899a94f9d097d74
midwinter1111 <kuro96fate@yahoo.co.jp>
2026-03-13 13:17:12 +0900

    [modify[ レイアウト修正

--- src/views/SamplePage.vue
+++ src/views/SamplePage.vue
@@ -1,7 +1,29 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { APP_PATH } from '../router'
+
+const router = useRouter()
+
+const goBack = () => {
+  router.push(APP_PATH.ROOT)
+}
+
+// エラー発生用関数
+const triggerError = () => {
+  throw new Error('テスト用の意図的なエラーが発生しました。')
+}
+</script>
+
 <template>
   <div class="about">
     <h1>Sample Page</h1>
   </div>
+  <div class="actions">
+      <button @click="goBack">Back to Home</button>
+      <button @click="triggerError" style="background-color: #ffcccc; margin-left: 10px;">
+        Trigger Error
+      </button>
+    </div>
 </template>
 
 <style>
@@ -11,5 +33,8 @@
     display: flex;
     align-items: center;
   }
+}
+.actions {
+  margin-top: 20px;
 }
 </style>
